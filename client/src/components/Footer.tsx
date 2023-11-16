591b105b3c2b10603b8ad14afa31deccc1c3b804
sasafokin2@gmail.com <PC@JODIFO-LAPTOP>
2023-11-16 17:03:14 +0300

    #1U: - added Footer component - added discord logo - added instagram logo - added twitter logo

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,56 @@
+import InstagramLogo from "../images/instagram-logo.png";
+import DiscordLogo from "../images/discord.png";
+import TwitterLogo from "../images/twitter.png";
+
+function Footer() {
+  return (
+    <>
+      <div className="bg-slate-800 w-full h-[30vh] mt-3 flex justify-center">
+        <div className="w-[75%] h-full py-2 flex flex-col justify-between">
+          <div className="w-full h-full">
+            <div className="text-2xl text-gray-200 ml-2">
+              We in social media
+            </div>
+            <hr className="w-full h-px bg-gray-200 my-3" />
+            <div className="text-gray-200 ml-1 text-lg mb-2">
+              <img
+                src={InstagramLogo}
+                alt="instagram logo"
+                className="w-[1.75rem] inline"
+              />
+              <a href="#" className="ml-2">
+                Instagram
+              </a>
+            </div>
+            <div className="text-gray-200 ml-1 text-lg mb-2">
+              <img
+                src={DiscordLogo}
+                alt="instagram logo"
+                className="w-[1.75rem] inline"
+              />
+              <a href="#" className="ml-2">
+                Discord
+              </a>
+            </div>
+            <div className="text-gray-200 ml-1 text-lg mb-2">
+              <img
+                src={TwitterLogo}
+                alt="instagram logo"
+                className="w-[1.75rem] inline"
+              />
+              <a href="#" className="ml-2">
+                Twitter
+              </a>
+            </div>
+            <hr className="w-full h-px bg-gray-200 my-3" />
+            <div className="text-gray-200 font-light italic">
+              JoDiFo-blog-site 2023
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
