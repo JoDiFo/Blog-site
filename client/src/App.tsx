a36fc2f36b0839bab83ce17270f4ddb9f0cb5a4f
sasafokin2@gmail.com <PC@JODIFO-LAPTOP>
2023-11-22 11:40:16 +0300

    Added sending of login data to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,49 +3,53 @@ import { Home, Courses, Features, Tutorials, Login, Profile } from "./pages";
 import { Header } from "./components/index";
 import { useState, useEffect } from "react";
 
-const headerLinks = [
-  {
-    id: "header_1",
-    linkName: "Home",
-    linkTo: "",
-    isActive: true,
-  },
-  {
-    id: "header_2",
-    linkName: "Features",
-    linkTo: "features",
-    isActive: true,
-  },
-  {
-    id: "header_3",
-    linkName: "Tutorials",
-    linkTo: "tutorials",
-    isActive: true,
-  },
-  {
-    id: "header_4",
-    linkName: "Courses",
-    linkTo: "courses",
-    isActive: true,
-  },
-];
+const SERVER_PORT = "http://localhost:8000/";
+
+type HeaderElement = {
+  id: string;
+  linkName: string;
+  linkTo: string;
+  isActive: boolean;
+};
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [headerLinks, setHeaderLinks] = useState<HeaderElement[]>([
+    { id: "0", linkName: "", linkTo: "", isActive: true },
+  ]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log("App started");
+    getHeaderElements();
   }, []);
 
-  const handleLogin = (emailValue: string, passwordValue: string) => {
+  const getHeaderElements = async () => {
+    const url = SERVER_PORT + "headers";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setHeaderLinks(data))
+      .catch((err) => console.log(err));
+  };
+
+  const handleLogin = async (emailValue: string, passwordValue: string) => {
     if (emailValue === "" && passwordValue === "") {
       alert("Please enter your email and password.");
       return;
     }
 
-    // log user in the data base or create new user
-    console.log("Log in", emailValue, passwordValue);
+    const url = SERVER_PORT + "login";
+
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: emailValue, password: passwordValue }),
+    })
+      .then((res) => {
+        console.log(res.status);
+      })
+      .catch((err) => console.log(err));
 
     setLoggedIn(true);
     navigate("");
--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const PORT = 8000;
 
 const app = express();
-app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 app.use("/images", express.static(__dirname + "/images"));
 
-// Default page
 app.get("/", async (_req, res) => {
   res.send("hello");
 });
 
-// Endpoint with courses data
 app.get("/courses/data", async (_req, res) => {
   res.sendFile("./Data/Courses.json", { root: __dirname });
 });
@@ -22,7 +23,15 @@ app.get("/articles", async (_req, res) => {
   res.sendFile("./Data/Articles.json", { root: __dirname });
 });
 
-// Not found
+app.get("/headers", async (_req, res) => {
+  res.sendFile("./Data/HeaderLinks.json", { root: __dirname });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send({ id: "user" });
+});
+
 app.use((_req, res) => {
   res.send("404 - Not found");
 });
